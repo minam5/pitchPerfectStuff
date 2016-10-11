57e70bdebc89825045a2f173493106058a92208b
minam5 <mina.mahmood@menloschool.org>
2016-10-11 14:58:26 -0700

    update playSoundsViewController

--- theRealPitchPerfect/PlaySoundsViewController.swift
+++ theRealPitchPerfect/PlaySoundsViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+   
+    var recordedAudioURL: NSURL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {case Slow = 0, Fast, Chipmunk, Vader, Echo, Reverb}
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        switch(ButtonType(rawValue: sender.tag)!){
+        case .Slow:
+            playSound(rate: 0.5)
+        case .Fast:
+            playSound(rate: 1.5)
+        case .Chipmunk:
+            playSound(rate: 1000)
+        case .Vader:
+            playSound(rate: -1000)
+        case .Echo:
+            playSound(echo: true)
+        case .Reverb:
+            playSound(reverb: true)
+        }
+        
+        
+        print("Play Sound Button Pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+        stopAudio()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupAudio()
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +65,21 @@ class PlaySoundsViewController: UIViewController {
     }
     
 
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "stopRecording"){
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! NSURL
+            //print("recordedAudioURL is: ", recordedAudioURL)
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI(playState: .NotPlaying)
+    }
+
+    
     /*
     // MARK: - Navigation
 
